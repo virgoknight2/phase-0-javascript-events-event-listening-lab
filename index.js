964f99e822ef857e657bbc8cfd49e2d22b14997d
Lorelai Knutti <virgoknight2@gmail.com>
2022-04-12 06:44:56 -0400

    commit tree 79ba528cd878e05d56bdcf5dd661f5bc28cab267

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
+//grabs the input field
+const input =document.getElementById(`input`)
+//adds a pop up alert when something is clicked
+//function addingEventListener() {
+//    alert(`click clack click clack`)
+//}
+//teaches the computer what event it's looking for as an input method
+//input.addEventListener(`click`,addingEventListener)
+ 
+//above code but all in one function
+//input.addEventListener(`click`, function addingEventListener(){
+//    alert(`click clack click clack`)
+//});
+function addingEventListener(){
+    input.addEventListener(`click`,function (){
+     return alert(`I was clicked`)
+    });
 }
